4f4907d73e0b72bc05897610fad698ea33320e84
cjwales <cjwales@gmail.com>
2019-11-06 10:15:56 +0000

    guest and guest test complete

--- src/test/java/GuestTest.java
+++ src/test/java/GuestTest.java
@@ -1,2 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuestTest {
+
+    Guest guest1;
+    Guest guest2;
+    Guest guest3;
+
+
+    @Before
+    public void before() {
+        guest1 = new Guest("Jordan");
+        guest2 = new Guest("Niall");
+        guest3 = new Guest("Steve");
+    }
+
+    @Test
+    public void guestHasName() {
+        assertEquals("Jordan", guest1.getName());
+    }
 }
